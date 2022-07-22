39e7b77afe0a7a1a5938f94e1aa34742c7fdd291
Chris Woodby <chriswoodby@Chriss-MacBook-Pro.local>
2022-07-22 10:22:18 -0500

    home page design phase 1 complete

--- client/Home.jsx
+++ client/Home.jsx
@@ -1,25 +1,55 @@
 import React, {useState} from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
+import background from '../media/background.png';
+import wedding from '../media/wedding.jpeg'
 
 const Home = function(props) {
     return (
       <div class="home">
-        <div class="banner">
-          <h2>home screen content</h2>
+        <div style={{backgroundImage: `url(${wedding})`, backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}} class="banner">
+          <div class="rsvp-content">
+            <h1>RSVP Now</h1>
+            <a>Click Here</a>
+          </div>
         </div>
         <div class="timeline">
-          <h3>schedule content here</h3>
-          <Row>
-            <Col>Guests arrive</Col>
-            <Col>ceremony</Col>
-            <Col>cocktail hour</Col>
-            <Col>Sunset</Col>
-            <Col>Grand Entrance/first dance</Col>
-            <Col>Dinner served</Col>
-            <Col>Speeches/Cake/Family dances </Col>
-            <Col>send off</Col>
-          </Row>
+          <Carousel variant="dark">
+            <Carousel.Item>
+              <img
+              className="d-block w-100"
+              src={background}
+              alt="First slide"
+              />
+              <Carousel.Caption>
+                <h5>First slide label</h5>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+              className="d-block w-100"
+              src={background}
+              alt="Second slide"
+              />
+              <Carousel.Caption>
+                <h5>Second slide label</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={background}
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h5>Third slide label</h5>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </div>
       </div>
     )
